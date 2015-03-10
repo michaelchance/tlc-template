3f56a61ff929b0c924896ab7975f4d6697b261d5
Michael <michaelc@zoovy.com>
2015-03-09 20:23:10 -0700

    fixes

--- tlc-template.js
+++ tlc-template.js
@@ -9,38 +9,52 @@ function TemplateModule(tlc){
 	var templates = {};
 	
 	this.loadTemplatesFromFile = function(filename){
-		fs.readFile(filename,function(err,result){
-			var $ = cheerio.load(text);
-			$('[id]').each(function(i,e){
-				templates[$(this).attr('id')] = $(this).removeAttr('id');
-				});
+		fs.readFile(filename,'utf-8',function(err,result){
+			if(err){
+				console.error(err);
+				}
+			else if (result){
+				var $ = cheerio.load(result);
+				$('[id]').each(function(i,e){
+					templates[$(this).attr('id')] = $.html($(this).removeAttr('id'));
+					});
+				}
 			});
 		}
 	this.loadTemplates = function(htmlString){
+		
 		var $ = cheerio.load(htmlString);
 		$('[id]').each(function(i,e){
-			templates[$(this).attr('id')] = $(this).removeAttr('id');
+			templates[$(this).attr('id')] = $.html($(this).removeAttr('id'));
 			});
 			
 		}
 		
 	this.tlcModule = {
-		translate : function(tlc){
-			var data = tlc.args('data');
+		translate : function(context){
+			var data = context.args('data');
+			console.dir(data);
 			if(data){
-				var templateid = tlc.args('templateid');
+				var templateid = context.args('templateid');
+				console.dir(templates);
+				var $tag = context.$focus();
 				if(templateid){
-					$tag.append(templates[templateid].html());
+					console.log(templates[templateid]);
+					$tag.append(templates[templateid]);
+					console.log('post append');
+					console.log($tag.html());
 					}
+				console.dir(tlc);
 				return tlc.run($tag,data);
 				}
 			else{
 				return false;
 				}
 			},
-		append : function(tlc){
-			var templateid = tlc.args('templateid');
+		append : function(context){
+			var templateid = context.args('templateid');
 			if(templateid){
+				var $tag = context.$focus();
 				return $tag.append(templates[templateid].html());
 				}
 			else{
